04b573166d092571f469579eeac49fa015a5d088
Dave Antoine <thewizrd.dev@gmail.com>
2022-03-10 11:42:55 -0500

    WebConfig: configure CORS for local Angular application

--- src/main/java/com/cogent/fooddeliveryapp/FoodDeliveryAppApplication.java
+++ src/main/java/com/cogent/fooddeliveryapp/FoodDeliveryAppApplication.java
@@ -2,12 +2,26 @@ package com.cogent.fooddeliveryapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FoodDeliveryAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryAppApplication.class, args);
+	}
+	
+	/* Add CORS mapping for Angular application */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
